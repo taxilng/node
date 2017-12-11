b0151addf2ed2eb0ccfde7a86644a21a970b7340
telanks <769967440@qq.com>
2017-12-11 22:08:19 +0800

    异步读取文件

--- models/optfile.js
+++ models/optfile.js
@@ -1,1 +1,19 @@
 const fs = require('fs')
+module.exports = {
+    readFileSync: (path)=>{
+        const data = fs.readFileSync(path,'utf-8')
+        console.log(data);
+        console.log('同步方法执行完毕');
+    },
+    readFile: (path, recall)=>{
+        fs.readFile(path,(err, data)=>{
+            if(err){
+                console.log(err);
+            }else{
+                console.log(data.toString());
+                recall(data)
+            }
+        })
+        console.log('异步方法执行完毕');
+    }
+}
--- n1.js
+++ n1.js
@@ -1,5 +1,16 @@
 const http = require('http')
 const optfile = require('./models/optfile')
+let i = 0;
 http.createServer((req, res)=>{
-
+    res.writeHead(200, {'Content-Type':'text/html; charset=utf-8'})
+    if(req.url !== '/favicon.ico'){ //清除第二次访问
+        // optfile.readFileSync('./views/login.html')
+        optfile.readFile('./views/login.html',recall)
+        function recall(data) {
+            res.write(data)
+            res.end() //不写则没有http协议尾
+        }
+        console.log('主程序执行完毕'+i++);
+    }
 }).listen(8000)
+console.log('server running');
